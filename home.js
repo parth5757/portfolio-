a0c21b2a2eee708fe90179eec946db995f9bdf8e
parth5757 <psthakkar2@gmail.com>
2021-03-20 03:22:04 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,14 @@
+console.log('hello');
+// alert('hi parth');
+ 
+var p = 'parth code'
+console.log(p)
+
+no = 57;
+console.log(no);
+ 
+hobbie = prompt('what is your hobbie');
+
+document.getElementById('something').innerHTML = "<h1> parth is good coder </h1>";
+
+document.getElementById('special').innerHTML = hobbie
